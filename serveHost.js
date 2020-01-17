5e6cf590fa33130e5facf9005f2074cb6921ae23
ricou12 <dwwm2019@gmail.com>
2020-01-17 11:58:50 +0100

    modif

--- serveHost.js
+++ serveHost.js
@@ -71,14 +71,14 @@ const generateList = (title,note) => {
     return `
         <li class="UploadList-li border rounded mb-2 p-2">
             <div class="row">
-                <div class="col-2">
+                <div class="col-12 col-md-2">
                     <input type="checkbox" id="titleUploaded">
                     <label for="titleUploaded">${title}</label>
                 </div>
-                <div class="col">
+                <div class="col-12 col-md-auto">
                     <p>${note}</p>
                 </div>
-                <div class="col-2">
+                <div class="col-12 col-md-2">
                     <button>dell</button>
                 </div>
             </div>
